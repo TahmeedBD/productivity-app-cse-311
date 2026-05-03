401f60fc0dde408dbbe9c902f018bd0b3873cca7
tahmeedbd <tahmeedv2@gmail.com>
2026-05-03 09:08:00 +0600

    feat: initialize base application entrypoints

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+echo '<h1>Welcome to Productivity App</h1>';
+echo '<p>PHP and MySQL setup with Docker is working!</p>';
+?>
+<script src="/js/main.js"></script>
---
+++ src/ts/main.ts
@@ -0,0 +1,1 @@
+console.log('TypeScript is successfully compiled and running!');
